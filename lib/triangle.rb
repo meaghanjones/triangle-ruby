8101bbc2679e96e0c2df96b0a9394bc6b757b872
Meaghan Jones and Maldon Meehan <maldon@>
2016-07-05 10:21:17 -0700

    retyrns true if it is a scalene triangle passing  test

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,7 +17,7 @@ class Triangle
     (@side1 == @side2 && @side1 != @side3) || (@side1 == @side3 && @side1 != @side2) || (@side2 == @side3 && @side2 != @side1)
   end
 
-  # define_method(:scalene?) do
-  #   @side1 != @side2 && @side1 != @side3 && @side2 != @side3
-  # end
+  define_method(:scalene?) do
+    @side1 != @side2 && @side1 != @side3 && @side2 != @side3
+  end
 end
