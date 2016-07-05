77f91cf1d5e19c500c2259a029bc928babf9707f
Meaghan Jones and Maldon Meehan <maldon@>
2016-07-05 10:03:39 -0700

    return true if it is an isosceles triangle failing test

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -7,12 +7,17 @@ describe(Triangle) do
       test_triangle = Triangle.new(1, 1, 15)
       expect(test_triangle.triangle?()).to(eq(false))
     end
-
+  end
     describe("#equilateral?") do
       it("returns true if it is an equilateral tirangle")do
         test_equilateral = Triangle.new(3, 3, 3)
         expect(test_equilateral.equilateral?()).to(eq(true))
       end
     end
-  end
+    describe("#isosceles?")do
+      it("returns true if it is an isosceles triangle")do
+        test_isosceles = Triangle.new(10, 10, 5)
+        expect(test_isosceles.isosceles?()).to(eq(true))
+      end
+    end
 end
