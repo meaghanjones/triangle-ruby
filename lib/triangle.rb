d07db1a546b58a9dc5eec991f759d8d24dcc5913
Meaghan Jones and Maldon Meehan <meaghan@>
2016-07-05 10:20:34 -0700

    retyrns true if it is a scalene triangle failing test

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,4 +16,8 @@ class Triangle
   define_method(:isosceles?) do
     (@side1 == @side2 && @side1 != @side3) || (@side1 == @side3 && @side1 != @side2) || (@side2 == @side3 && @side2 != @side1)
   end
+
+  # define_method(:scalene?) do
+  #   @side1 != @side2 && @side1 != @side3 && @side2 != @side3
+  # end
 end
--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -3,21 +3,27 @@ require('triangle')
 
 describe(Triangle) do
   describe("#triangle?") do
-    it("returns false if it is not a triangle")do
+    it("returns false if it is not a triangle") do
       test_triangle = Triangle.new(1, 1, 15)
       expect(test_triangle.triangle?()).to(eq(false))
     end
   end
     describe("#equilateral?") do
-      it("returns true if it is an equilateral tirangle")do
+      it("returns true if it is an equilateral tirangle") do
         test_equilateral = Triangle.new(3, 3, 3)
         expect(test_equilateral.equilateral?()).to(eq(true))
       end
     end
-    describe("#isosceles?")do
-      it("returns true if it is an isosceles triangle")do
+    describe("#isosceles?") do
+      it("returns true if it is an isosceles triangle") do
         test_isosceles = Triangle.new(10, 10, 5)
         expect(test_isosceles.isosceles?()).to(eq(true))
+      end
+    end
+    describe("#scalene?") do
+      it("returns true if it is a scalene triangle") do
+        test_scalene = Triangle.new(5, 6, 10)
+        expect(test_scalene.scalene?()).to(eq(true))
       end
     end
 end
